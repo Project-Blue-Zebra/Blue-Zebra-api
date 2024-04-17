607578fe0d57f6cb2a3ec66fd1187434a341f39e
Grace Dockery <gdockery19@gmail.com>
2024-04-17 00:03:11 -0400

    Modified Catalog Controller with Items (p 10)

--- blue.zebra.Api/Controllers/CatalogController.cs
+++ blue.zebra.Api/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using blue.zebra.Domain.Catalog;
+using System.Net.Http.Headers;
 
 namespace blue.zebra.Controllers
 {
@@ -8,7 +9,11 @@ namespace blue.zebra.Controllers
     public class CatalogController : ControllerBase{
         [HttpGet]
         public IActionResult GetItems(){
-            return Ok("Hello World!");
+            var items = new List<Item>(){
+                new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State Shorts", "Nike", 44.99m)
+            };
+            return Ok(items);
         }
 
     }
